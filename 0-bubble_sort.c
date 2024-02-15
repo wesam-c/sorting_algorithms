7eadc6cacf19d2cb8979ba86d4c82f5b481af49a
Abraham Paul <codexapp44@gmail.com>
2024-02-15 03:39:48 -0800

    task 0 completed, bubble sort function created. created a main.c file for testing the function
    bubble_sort function passed all test and meets requirements as stipulated on the task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,5 +8,19 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	//Code goes here
+	unsigned int i,j;
+	
+	for(i = 0; i < size - 1; i++)
+	{
+		for(j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+                		int temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+                	}
+		}
+	}
 }
