8e160723f7ae6c13e0e32cd2546e0ab3839a511c
Abraham Paul <codexapp44@gmail.com>
2024-02-15 02:45:49 -0800

    created the prototype for the bubble_sort function in task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorts an array of integers in ascending order
+* 		using the Bubble sort algorithm
+* @array: array to sort using the bubble sort algorithm
+* @size: size of the array(number of elements in the array)
+*/
+void bubble_sort(int *array, size_t size)
+{
+	//Code goes here
+}
