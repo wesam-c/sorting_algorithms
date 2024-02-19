42e838e7ee05c63ad00d92af666f65b97866b8c2
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-02-19 10:41:59 +0000

    working on task 103-merge sort

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
