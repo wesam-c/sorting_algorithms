7edf43b46caba45b647a3e596379b92234be49d2
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-02-19 10:25:29 +0000

    task 101-cocktail_sort complete

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void selection_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
